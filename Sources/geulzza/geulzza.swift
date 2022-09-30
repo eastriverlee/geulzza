2804e686615615a8bb6110c0673f32697849c286
eastriver <eastriver0720@gmail.com>
2022-10-01 08:53:53 +0900

    fix initializer && replace "__" to "x"

--- Sources/geulzza/geulzza.swift
+++ Sources/geulzza/geulzza.swift
@@ -3,7 +3,7 @@ public typealias Hex = UInt16
 public typealias Hangeul = String
 public typealias Jamo = Character
 
-public enum Jaeum: Jamo {
+public enum Jaeum: Jamo, CaseIterable {
     case ㄱ      = "ᄀ"
     case ㄲ      = "ᄁ"
     case ㄴ      = "ᄂ"
@@ -23,10 +23,15 @@ public enum Jaeum: Jamo {
     case ㅌ      = "ᄐ"
     case ㅍ      = "ᄑ"
     case ㅎ      = "ᄒ"
-    case __      = " "
+    case x       = " "
+    static var random: Jaeum {
+        let allCases = Jaeum.allCases
+        let randomIndex = (0..<allCases.firstIndex(of: .ㅎ)!).randomElement()!
+        return allCases[randomIndex]
+    }
 }
 
-public enum Moeum: Jamo {
+public enum Moeum: Jamo, CaseIterable {
     case ㅏ      = "ᅡ"
     case ㅐ      = "ᅢ"
     case ㅑ      = "ᅣ"
@@ -48,10 +53,15 @@ public enum Moeum: Jamo {
     case ㅡ      = "ᅳ"
     case ㅢ      = "ᅴ"
     case ㅣ      = "ᅵ"
-    case __      = " "
+    case x       = " "
+    static var random: Moeum {
+        let allCases = Moeum.allCases
+        let randomIndex = (0..<allCases.firstIndex(of: .ㅣ)!).randomElement()!
+        return allCases[randomIndex]
+    }
 }
 
-public enum Badchim: Jamo {
+public enum Badchim: Jamo, CaseIterable {
     case ㄱ      = "ᆨ"
     case ㄲ      = "ᆩ"
     case ㄱㅅ    = "ᆪ"
@@ -79,9 +89,64 @@ public enum Badchim: Jamo {
     case ㅌ      = "ᇀ"
     case ㅍ      = "ᇁ"
     case ㅎ      = "ᇂ"
-    case __      = " "
+    case x       = " "
+    static var random: Badchim {
+        let allCases = Badchim.allCases
+        let randomIndex = (0..<allCases.firstIndex(of: .ㅎ)!).randomElement()!
+        return allCases[randomIndex]
+    }
 }
 
+public extension Jaeum {
+    var badchim: Badchim {
+        switch self {
+        case .ㄱ: return .ㄱ
+        case .ㄲ: return .ㄲ
+        case .ㄴ: return .ㄴ
+        case .ㄷ: return .ㄷ
+        case .ㄸ: return .ㄷ
+        case .ㄹ: return .ㄹ
+        case .ㅁ: return .ㅁ
+        case .ㅂ: return .ㅂ
+        case .ㅃ: return .ㅂ
+        case .ㅅ: return .ㅅ
+        case .ㅆ: return .ㅆ
+        case .ㅇ: return .ㅇ
+        case .ㅈ: return .ㅈ
+        case .ㅉ: return .ㅈ
+        case .ㅊ: return .ㅊ
+        case .ㅋ: return .ㅋ
+        case .ㅌ: return .ㅌ
+        case .ㅍ: return .ㅍ
+        case .ㅎ: return .ㅎ
+        case .x : return .x
+        }
+    }
+    var pretty: Character {
+        switch self {
+        case .ㄱ: return "ㄱ"
+        case .ㄲ: return "ㄲ"
+        case .ㄴ: return "ㄴ"
+        case .ㄷ: return "ㄷ"
+        case .ㄸ: return "ㄸ"
+        case .ㄹ: return "ㄹ"
+        case .ㅁ: return "ㅁ"
+        case .ㅂ: return "ㅂ"
+        case .ㅃ: return "ㅃ"
+        case .ㅅ: return "ㅅ"
+        case .ㅆ: return "ㅆ"
+        case .ㅇ: return "ㅇ"
+        case .ㅈ: return "ㅈ"
+        case .ㅉ: return "ㅉ"
+        case .ㅊ: return "ㅊ"
+        case .ㅋ: return "ㅋ"
+        case .ㅌ: return "ㅌ"
+        case .ㅍ: return "ㅍ"
+        case .ㅎ: return "ㅎ"
+        case .x : return " "
+        }
+    }
+}
 public extension Badchim {
     var 앞: Badchim {
         switch self {
@@ -98,7 +163,7 @@ public extension Badchim {
         case .ㄹㅎ : return .ㄹ
         case .ㅂㅅ : return .ㅂ
         case .ㅆ   : return .ㅅ
-        default    : return .__
+        default    : return .x
         }
     }
     var 뒤: Badchim {
@@ -143,14 +208,15 @@ let moeumStart   : Hex = 0x1161
 let badchimStart : Hex = 0x11A7
 public extension Character {
     init(_ 초성: Jaeum, _ 중성: Moeum, _ 종성: Badchim) {
-        guard 중성 != .__ && 종성 != .__ else { self = 초성.rawValue; return }
+        guard 초성 != .x || 중성 != .x || 종성 != .x else { self = " "; return }
+        guard 중성 != .x else { self = 초성.pretty; return }
         let cho = 초성.rawValue.unicode - jaeumStart
         let jung = 중성.rawValue.unicode - moeumStart
-        let zong = 종성.rawValue.unicode - badchimStart
+        let zong = 종성 == .x ? 0 : 종성.rawValue.unicode - badchimStart
         var unicode: Hex
 
         unicode = hangeulStart + (cho * 21 + jung) * 28
-        if 종성 != .__ { unicode += zong }
+        unicode += zong
         self = unicode.char
     }
     var geulzza: Geulzza {
@@ -160,10 +226,10 @@ public extension Character {
 
 public extension Hangeul {
     var endsWithBadchim: Bool {
-        switch last { 
+        switch last {
             case "b", "c", "d", "g", "k", "l", "m", "n", "p", "q", "t": return true
             case "B", "C", "D", "G", "K", "L", "M", "N", "P", "Q", "T": return true
-            default: return last != nil && last!.geulzza.받침 != .__
+            default: return last != nil && last!.geulzza.받침 != .x
         }
     }
     var 은는: Hangeul {
@@ -184,19 +250,19 @@ public class Geulzza {
         let offset = unicode - hangeulStart
         초성 = Jaeum(rawValue: ((offset / 28) / 21 + jaeumStart).char)!
         중성 = Moeum(rawValue: ((offset / 28) % 21 + moeumStart).char)!
-        종성 = Badchim(rawValue: ((offset % 28) + badchimStart).char) ?? .__
+        종성 = Badchim(rawValue: ((offset % 28) + badchimStart).char) ?? .x
         char = Character(초성, 중성, 종성)
     }
     var unicode: Hex
     var nonHangeul: Character?
     var char: Character = " "
-    var 초성: Jaeum = .__ {
+    var 초성: Jaeum = .x {
         didSet { char = Character(초성, 중성, 종성) }
     }
-    var 중성: Moeum = .__ {
+    var 중성: Moeum = .x {
         didSet { char = Character(초성, 중성, 종성) }
     }
-    var 종성: Badchim = .__ {
+    var 종성: Badchim = .x {
         didSet { char = Character(초성, 중성, 종성) }
     }
     var 받침: Badchim {
@@ -212,6 +278,6 @@ extension Geulzza: Equatable {
         lhs.char == rhs
     }
     public static func == (lhs: Character, rhs: Geulzza) -> Bool {
-        lhs == rhs.char 
+        lhs == rhs.char
     }
 }
